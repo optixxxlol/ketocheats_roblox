64d0353a12d86f7a54e134aca553cbcd1cfe428a
optixxxlol <102333416+optixxxlol@users.noreply.github.com>
2022-12-07 22:13:53 -0600

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+-- fucking retard go away :)
+if game.GameId == "3233893879" then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/optixxxlol/ketocheats_roblox/main/scripts/bb.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/optixxxlol/ketocheats_roblox/main/scripts/universal.lua"))()
+end
