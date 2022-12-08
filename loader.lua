99a35f0e1b9124bee74db26a368d64f3e349da2a
optixxxlol <102333416+optixxxlol@users.noreply.github.com>
2022-12-08 07:31:04 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 -- fucking retard go away :)
-if game.PlaceId == "3233893879" then
+if game.GameId == "1168263273" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/optixxxlol/ketocheats_roblox/main/scripts/bb.lua"))()
 else
   loadstring(game:HttpGet("https://raw.githubusercontent.com/optixxxlol/ketocheats_roblox/main/scripts/universal.lua"))()
